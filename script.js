d62d7cca45972c102336e71cce4322a8b5bf9684
floggie23 <boomb2323@gmail.com>
2022-06-29 16:33:03 +0200

    fs

---
+++ script.js
@@ -0,0 +1,18 @@
+
+ 
+function findOdds(nr){
+    var nr1;
+    for(var i = 1 ; i <= nr; i++ ){
+        if (i%2 == 1 ) {
+            nr1 = i;
+
+            nr1++;
+            
+            console.log(i)
+        }
+    }
+}
+
+findOdds(10);
+
+
